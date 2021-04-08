eb6d53f8813592babe6c4668a932b30fa4de5a8a
goodchuck <79144494+goodchuck@users.noreply.github.com>
2021-04-08 17:59:46 +0900

    21/04/08 서블릿/jsp 강의 23강 수강

--- JSPPrj/src/com/newlecture/web/Calc.java
+++ JSPPrj/src/com/newlecture/web/Calc.java
@@ -1,4 +1,4 @@
-package com.newlecture.test;
+package com.newlecture.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,8 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("/add")
-public class Add extends HttpServlet{
+@WebServlet("/calc")
+public class Calc extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, 
 			HttpServletResponse response) throws ServletException, IOException {
@@ -20,13 +20,19 @@ public class Add extends HttpServlet{
 		
 		String x_ = request.getParameter("x");
 		String y_ = request.getParameter("y");
+		String op = request.getParameter("operator");
 		int x = 0;
 		int y = 0;
 		
 		if(!x_.equals("")) x = Integer.parseInt(x_);
 		if(!y_.equals("")) y = Integer.parseInt(y_); 
 		
-		int result = x+y;
+		int result = 0;
+		
+		if(op.equals("µ¡¼À"))
+			result = x+y;
+		else
+			result = x-y;
 		out.printf("result is %d\n", result);
 	}
 }

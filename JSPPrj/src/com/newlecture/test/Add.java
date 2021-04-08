310886f6fd8c01cea64d221255c8062934827899
goodchuck <79144494+goodchuck@users.noreply.github.com>
2021-04-08 17:50:13 +0900

    21/04/08 서블릿/jsp 강의 22강 수강

--- JSPPrj/src/com/newlecture/test/Add.java
+++ JSPPrj/src/com/newlecture/test/Add.java
@@ -9,19 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/add")
-public class Test extends HttpServlet{
+public class Add extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, 
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html); charset=UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 		
 		PrintWriter out = response.getWriter();
 		
-		String x = request.getParameter("x");
-		String y = request.getParameter("y");
-		int x1 = Integer.parseInt(x);
-		int y1 = Integer.parseInt(y);
-		out.println(x1 + y1);
+		String x_ = request.getParameter("x");
+		String y_ = request.getParameter("y");
+		int x = 0;
+		int y = 0;
+		
+		if(!x_.equals("")) x = Integer.parseInt(x_);
+		if(!y_.equals("")) y = Integer.parseInt(y_); 
+		
+		int result = x+y;
+		out.printf("result is %d\n", result);
 	}
 }

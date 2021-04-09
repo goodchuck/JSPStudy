f745dbd0e212d555f3d0a2fa74dc50fc0c1855d8
goodchuck <79144494+goodchuck@users.noreply.github.com>
2021-04-09 20:01:16 +0900

    21/04/09 서블릿/jsp 강의 37강 수강

--- JSPPrj/src/com/newlecture/web/Calc3.java
+++ JSPPrj/src/com/newlecture/web/Calc3.java
@@ -42,6 +42,9 @@ public class Calc3 extends HttpServlet{
 				e.printStackTrace();
 			}
 		}
+		else if(operator != null && operator.equals("C")) {
+			exp = "";
+		}
 		else {
 			exp += (value == null) ? "": value;
 			exp += (operator == null) ? "": operator;
@@ -51,7 +54,8 @@ public class Calc3 extends HttpServlet{
 		
 		
 		Cookie expCookie = new Cookie("exp", exp);
-		
+		if(operator != null && operator.equals("C")) 
+			expCookie.setMaxAge(0);
 		response.addCookie(expCookie);
 		response.sendRedirect("calcpage");
 
